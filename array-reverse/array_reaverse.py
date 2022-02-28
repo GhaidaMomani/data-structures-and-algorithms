46168cde5767d6ad0727684b2dc06a545c6d2e69
GhaidaMomani <ghaidaalmomani3@gmail.com>
2022-02-28 22:07:14 +0200

    ch01

---
+++ array-reverse/array_reaverse.py
@@ -0,0 +1,11 @@
+
+list = [1, 2 ,3, 4, 5]  ## input 
+
+def reverse_lst(arr):
+  new_list = []
+  for i in range(1, len(list)+1):
+    new_list.append(list[-i])
+    
+  print(new_list)
+
+reverse_lst(list)
